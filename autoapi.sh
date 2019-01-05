079b158e6b18e402d8b0bcb048efcaa69d878965
MaStEr FeRi <feri.sys13@Gmail.com>
2019-01-04 16:49:09 -0800

    Update autoapi.sh

--- autoapi.sh
+++ autoapi.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-killall tg
-killall nohup
-killall .telegram-cli
-killall apimode
-rm -rf /root/open/.telegram-cli/apimode
 ./api.sh
 let COUNTER=COUNTER+1 
 done
